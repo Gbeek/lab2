a8bf7c9ee69fc6e1f409590e257fae9dae07692a
Gbeek <Gbeek337@gmail.com>
2024-12-29 19:51:41 +0300

    Улучш кальк

--- commit/calc pr.py
+++ commit/calc pr.py
@@ -1,6 +1,10 @@
 def add(x, y):
     return x + y
 
+def subtract(x, y):
+    return x - y
+
 if __name__ == "__main__":
     print("Простой калькулятор")
     print("1 + 2 =", add(1, 2))
+    print("5 - 3 =", subtract(5, 3))
